78ccb97cbb8c6b9c93b1815966a5946b0f3f52f0
8pxl <56054380+8pxl@users.noreply.github.com>
2025-07-11 00:05:32 -0400

    add copy button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import {LinkBox} from "@/app/linkBox"
-import { Result } from "./result";
+import { useRouter } from "next/router";
 import {useState} from 'react';
 import { answer } from "./answer";
 import { Button } from "@/components/ui/button"
+import { CopyIcon } from "@/app/icon/copy"
 // import {Input} from "@/components/ui/input";
 
 function HalfWrapper({children, css}: any) {
@@ -13,6 +14,7 @@ function HalfWrapper({children, css}: any) {
     </div>
   )
 }
+
 export default function Home() {
   const [hidden, setHidden] = useState(true);
   const [response, setResponse] = useState("");
@@ -34,12 +36,13 @@ export default function Home() {
           </HalfWrapper>
             
             
-          <HalfWrapper css={"duration-300 ease-out text-gray-300 flex " + (hidden ? "pre-anim" : "")}>
+          <HalfWrapper css={"duration-300 ease-out text-gray-300 flex items-center justify-between " + (hidden ? "pre-anim" : "")}>
             {/* <LinkBox/> */}
             {response}
-            <Button variant="secondary" size="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"/></svg>
-              {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+            <Button className={"hover:scale-115" } variant="ghost" onClick={()=>{
+              navigator.clipboard.writeText(response);
+            }} size="icon">
+              {<CopyIcon/>}
             </Button>
           </HalfWrapper>
         </div>
