59e0aa08acbd799487d3434e61dbd02be2ddd113
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-11 16:40:06 +0800

    update next & make form eager

--- app/linkBox.tsx
+++ app/linkBox.tsx
@@ -1,12 +1,27 @@
-import {Input} from "@/components/ui/input";
- 
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 type LinkBoxProps = {
-    action: (formData: FormData) => Promise<void>;
+  action: (formData: FormData) => Promise<void>;
 };
-export function LinkBox({action} : LinkBoxProps) {
-    return(
-        <form action={action}>
-            <Input name="content" type="url" placeholder="paste your link here!"/>
-      </form>
-    )
+
+export function LinkBox({ action }: LinkBoxProps) {
+  const [url, setUrl] = useState("");
+  const handleSubmit = () => {
+    setUrl("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} action={action}>
+      <Input
+        name="content"
+        type="url"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="paste your link here!"
+      />
+    </form>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import {LinkBox} from "@/app/linkBox"
-import {useState} from 'react';
+import { LinkBox } from "@/app/linkBox"
+import { useState } from 'react';
 import { answer } from "./answer";
 import { Button } from "@/components/ui/button"
 import { CopyIcon } from "@/app/icon/copy"
@@ -11,8 +11,8 @@ type HalfWrapperProps = {
   css?: string;
 };
 
-function HalfWrapper({children, css} : HalfWrapperProps) {
-  return(
+function HalfWrapper({ children, css }: HalfWrapperProps) {
+  return (
     <div className={"w-[50%] " + css}>
       {children}
     </div>
@@ -23,37 +23,36 @@ export default function Home() {
   const [hidden, setHidden] = useState(true);
   const [response, setResponse] = useState("");
   return (
-      <div>
-        <div className="font-space font-bold text-gray-300 text-4xl w-screen justify-center flex m-4">
-          copify
+    <div>
+      <div className="font-space font-bold text-gray-300 text-4xl w-screen justify-center flex m-4">
+        copify
+      </div>
+      <div className="font-space font-bold text-gray-600 w-screen justify-center flex m-4">
+        <div className="w-[60%]">
+          a simple way to copy title/artists from spotify songs (cause for some reason you can&apos;t do it in spotify)
         </div>
-        <div className="font-space font-bold text-gray-600 w-screen justify-center flex m-4">
-          <div className="w-[60%]">
-            a simple way to copy title/artists from spotify songs (cause for some reason you can&apos;t do it in spotify)
-          </div>
-        </div>
-        <div className={"w-screen absolute top-1/2 flex content-center flex-col items-center gap-2"}>
-          <HalfWrapper>
-            <LinkBox
-            action={async (formData: FormData)=>{
+      </div>
+      <div className={"w-screen absolute top-1/2 flex content-center flex-col items-center gap-2"}>
+        <HalfWrapper>
+          <LinkBox
+            action={async (formData: FormData) => {
               const x = await answer(formData);
-              console.log(x);
               setResponse(x);
               setHidden(false);
             }}
-            />
-          </HalfWrapper>
-            
-          <HalfWrapper css={"duration-300 ease-out text-gray-300 flex items-center justify-between " + (hidden ? "pre-anim" : "")}>
-            {/* <LinkBox/> */}
-            {response}
-            <Button className={"hover:scale-115" } variant="ghost" onClick={()=>{
-              navigator.clipboard.writeText(response);
-            }} size="icon">
-              {<CopyIcon/>}
-            </Button>
-          </HalfWrapper>
-        </div>
+          />
+        </HalfWrapper>
+
+        <HalfWrapper css={"duration-300 ease-out text-gray-300 flex items-center justify-between " + (hidden ? "pre-anim" : "")}>
+          {/* <LinkBox/> */}
+          {response}
+          <Button className={"hover:scale-115"} variant="ghost" onClick={() => {
+            navigator.clipboard.writeText(response);
+          }} size="icon">
+            {<CopyIcon />}
+          </Button>
+        </HalfWrapper>
       </div>
+    </div>
   );
 }
